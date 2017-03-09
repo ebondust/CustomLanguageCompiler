934045b9ad49707385d53ddaa8960c15a847fb7b
Sebastian Gierłowski <ebondust@gmail.com>
2017-03-09 23:14:38 +0100

    Implementation of basic lexer.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SimpleTrainingCompiler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace CustomLanguageCompiler
 {
@@ -10,6 +12,13 @@ namespace CustomLanguageCompiler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Type in a mathematical expression: ");
+            string code = Console.ReadLine();
+            Compiler compiler = new Compiler();
+            compiler.Compile(code);
+            
+            Console.WriteLine(compiler.GetOutput());
+            Console.ReadLine();
         }
     }
 }
